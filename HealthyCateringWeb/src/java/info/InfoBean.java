92c4e45497a1be2ebda1ba9d2d20e9b888ae3515
Karl Overå <karloveraa@gmail.com>
2013-03-19 13:08:37 +0100

    Some basci design and a java class.

---
+++ HealthyCateringWeb/src/java/info/InfoBean.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info;
+
+/**
+ *
+ * @author Karl
+ */
+public class InfoBean {
+    
+}
