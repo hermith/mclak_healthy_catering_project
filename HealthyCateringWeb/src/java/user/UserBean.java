10d728661727319b76e05beaafcb044f7a65944d
l1nnk <linnkri91@gmail.com>
2013-04-09 12:14:11 +0200

    Loginpage fixed v2

--- HealthyCateringWeb/src/java/user/UserBean.java
+++ HealthyCateringWeb/src/java/user/UserBean.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.servlet.ServletException;
@@ -32,8 +33,35 @@ public class UserBean implements Serializable{
         } catch (ServletException ex) {
             Logger.getLogger(UserBean.class.getName()).log(Level.SEVERE, null, ex);
         }
-        Logger.getLogger(UserBean.class.getName()).log(Level.OFF, username + ", ny " + password);
         return "";
+    }
+    
+    public String logout() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        try {        
+            request.logout();
+        } catch (ServletException ex) {
+            Logger.getLogger(UserBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
+    }
+    /**
+     * MÅ LAGE METODE FOR ALLE BRUKERE NB NB NB NB NB
+     * @return 
+     */
+    public boolean isLoggedIn() {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        Object requestObject = context.getRequest();
+        if (requestObject instanceof HttpServletRequest) {
+            HttpServletRequest request = (HttpServletRequest) requestObject;
+            System.out.println(request.getRemoteUser());
+            if (request.getRemoteUser() != null) {
+                System.out.println("Is hear");
+                return true;
+            }
+        }
+        return false;
     }
 
     public String getUsername() {
