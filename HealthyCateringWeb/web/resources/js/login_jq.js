48e2cf10b365508dcad8d2fcc830e09a73212cfb
Karl Overå <karloveraa@gmail.com>
2013-04-09 23:48:05 +0200

    CSS cleanup.

--- HealthyCateringWeb/web/resources/js/login_jq.js
+++ HealthyCateringWeb/web/resources/js/login_jq.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
     var isUp = true;
 
-    $login_bar = $("#top_bar_span_login");
-
     $("#top_bar_span_login_always_show").click(function() {
+        
+        // Starting animation of drop down and white overlay.
         $("#site_opaque").fadeToggle('slow');
-        $login_bar.slideToggle('slow');
+        $("#top_bar_span_login").slideToggle('slow');
+        
+        // Enabling/disabling overflow (scroll bars)
         if (isUp) {
             $("body").css("overflow", "hidden");
             isUp = false;
         } else {
-            $login_bar.promise().done(function() {
-                $("body").css("overflow", "auto");
+            $("#top_bar_span_login").promise().done(function() {
+                $("body").css("overflow", "auto").fadeIn('slow');
                 isUp = true;
             });
         }
+        
     });
 });
