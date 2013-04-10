1e576bde74d9ea6b87dd3ca724c71dee45fc614c
Karl Overå <karloveraa@gmail.com>
2013-04-11 00:08:48 +0200

    First draft of the admin/employee panel

--- HealthyCateringWeb/web/resources/js/login_jq.js
+++ HealthyCateringWeb/web/resources/js/login_jq.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     var isUp = true;
 
     $("#top_bar_span_login_always_show").click(function() {
-        
+
         // Starting animation of drop down and white overlay.
         $("#site_opaque").fadeToggle('slow');
         $("#top_bar_span_login").slideToggle('slow');
-        
+
         // Enabling/disabling overflow (scroll bars)
         if (isUp) {
             $("body").css("overflow", "hidden");
@@ -17,6 +17,10 @@ $(document).ready(function() {
                 isUp = true;
             });
         }
-        
+
     });
+
+    /*$(".control_panel_header").click(function() {
+        $(".control_panel_button_section").slideToggle('slow');
+    });*/
 });
