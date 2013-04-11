66e5701fb5869104d2336f1dfaf825c66ea2969f
Christer Olsen <colsen1991@gmail.com>
2013-04-11 10:54:38 +0200

    Fiexed the userbean to work with a user object and not raw data such as strings

--- HealthyCateringWeb/src/java/user/UserBean.java
+++ HealthyCateringWeb/src/java/user/UserBean.java
@@ -21,8 +21,20 @@ import javax.servlet.http.HttpServletRequest;
 @Named
 @SessionScoped
 public class UserBean implements Serializable {
-
+    
     User user;
+
+    public UserBean() {
+        
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public String login() {
         FacesContext context = FacesContext.getCurrentInstance();
@@ -63,41 +75,5 @@ public class UserBean implements Serializable {
             }
         }
         return false;
-    }
-
-    public void createUser(String username, String password, String role_id) {
-        this.user = new User(username, password, role_id);
-    }
-
-    public String getUsername() {
-        return user.getUsername();
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole_id() {
-        return role_id;
-    }
-
-    public void setRole_id(String role_id) {
-        this.role_id = role_id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
     }
 }
