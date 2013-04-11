a7bf056ef88a4d69e0e60b7256b997f0b1a2e565
Karl Overå <karloveraa@gmail.com>
2013-04-11 10:25:51 +0200

    Locked bar in place when logged in.

--- HealthyCateringWeb/web/resources/js/login_jq.js
+++ HealthyCateringWeb/web/resources/js/login_jq.js
@@ -1,26 +1,32 @@
+var loggedIn = false;
+
 $(document).ready(function() {
     var isUp = true;
+    
+    loggedIn = document.getElementById("j_idt13:is_user_logged_in").value === "true";
 
     $("#top_bar_span_login_always_show").click(function() {
 
-        // Starting animation of drop down and white overlay.
-        $("#site_opaque").fadeToggle('slow');
-        $("#top_bar_span_login").slideToggle('slow');
+        if (!loggedIn) {
+            // Starting animation of drop down and white overlay.
+            $("#site_opaque").fadeToggle('slow');
+            $("#top_bar_span_login").slideToggle('slow');
 
-        // Enabling/disabling overflow (scroll bars)
-        if (isUp) {
-            $("body").css("overflow", "hidden");
-            isUp = false;
-        } else {
-            $("#top_bar_span_login").promise().done(function() {
-                $("body").css("overflow", "auto").fadeIn('slow');
-                isUp = true;
-            });
+            // Enabling/disabling overflow (scroll bars)
+            if (isUp) {
+                $("body").css("overflow", "hidden");
+                isUp = false;
+            } else {
+                $("#top_bar_span_login").promise().done(function() {
+                    $("body").css("overflow", "auto").fadeIn('slow');
+                    isUp = true;
+                });
+            }
         }
 
     });
 
     /*$(".control_panel_header").click(function() {
-        $(".control_panel_button_section").slideToggle('slow');
-    });*/
+     $(".control_panel_button_section").slideToggle('slow');
+     });*/
 });
