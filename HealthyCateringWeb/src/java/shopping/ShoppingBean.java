7bc1d94aba0895c32279f18171e14849c6bd0864
Aleksander L. Rasch <yakushi.san@gmail.com>
2013-04-16 11:11:34 +0200

    ShoopingBean updated

--- HealthyCateringWeb/src/java/shopping/ShoppingBean.java
+++ HealthyCateringWeb/src/java/shopping/ShoppingBean.java
@@ -21,12 +21,64 @@ public class ShoppingBean implements Serializable {
     private static final long serialVersionUID = 1L;
     private Customer customer;
     private ShoppingCart shoppingCart;
-    
+
     public ShoppingBean() {
         customer = new Customer();
         shoppingCart = new ShoppingCart();
     }
-    
+
+    public String getEmail() {
+        if (customer != null) {
+            return customer.getEmail();
+        }
+        return null;
+    }
+
+    public void setEmail(String email) {
+        if (customer != null) {
+            this.customer.setEmail(email);
+        }
+    }
+
+    public String getAddress() {
+        if (customer != null) {
+            return customer.getAddress();
+        }
+        return null;
+    }
+
+    public void setAddress(String address) {
+        if (customer != null) {
+            this.customer.setAddress(address);
+        }
+    }
+
+    public String getPhoneNumber() {
+        if (customer != null) {
+            return customer.getPhoneNumber();
+        }
+        return null;
+    }
+
+    public void setPhoneNumber(String phone) {
+        if (customer != null) {
+            this.customer.setPhoneNumber(phone);
+        }
+    }
+
+    public int getZipCode() {
+        if (customer != null) {
+            return customer.getZipCode();
+        }
+        return -1;
+    }
+
+    public void setZipCode(int zipCode) {
+        if (customer != null) {
+            this.customer.setZipCode(zipCode);
+        }
+    }
+
     /**
      * Gets the menu from the database.
      */
@@ -36,16 +88,17 @@ public class ShoppingBean implements Serializable {
         prod.add("du");
         return prod;
     }
-    
+
     /**
      * Add a product.
+     *
      * @return true/false
      */
     public boolean addProduct(Product product) {
         return false;
     }
-    
-    public ArrayList<Product> getActiveOrders(){
+
+    public ArrayList<Product> getActiveOrders() {
         return null;
     }
 }
