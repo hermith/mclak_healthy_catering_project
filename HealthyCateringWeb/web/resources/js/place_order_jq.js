83d9db0d3b0f26bd2d295aa49487984e0d005f11
Karl Overå <karloveraa@gmail.com>
2013-04-17 15:22:08 +0200

    Changed how subscripts are imported and fixed some main CSS.
    For subscripts (scripts that are only used in one site), look at
    active_orderx.html to see how to do it.

--- HealthyCateringWeb/web/resources/js/place_order_jq.js
+++ HealthyCateringWeb/web/resources/js/place_order_jq.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-    //$("#place_order_form:place_order_radio:0").click(function() {
-    //    $("#site_opaque").show('fast');
-    //});
+    
+    $("#place_order_form\\:place_order_radio\\:0").click(function() {
+        $("#place_order_delivery_date_form").slideUp('slow')
+    });
+    
+    $("#place_order_form\\:place_order_radio\\:1").click(function() {
+        $("#place_order_delivery_date_form").slideDown('slow');
+    });
 });
