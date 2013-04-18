46cfcf364cef9e6d7b154296a9d79f47029b57a9
Aleksander L. Rasch <yakushi.san@gmail.com>
2013-04-18 13:04:38 +0200

    Double validation fml

--- HealthyCateringWeb/web/resources/js/register_validation.js
+++ HealthyCateringWeb/web/resources/js/register_validation.js
@@ -3,13 +3,11 @@
  * and open the template in the editor.
  */
 
-function checkPass()
+function checkPassPrivate()
 {
     // Stores password fields as objects
-    var pass1 = document.getElementById('pass1');
-    var pass2 = document.getElementById('pass2');
-    // Stores message field as object
-    var message = document.getElementById('confirmMessage');
+    var pass1 = document.getElementById('registerprivate:pass1');
+    var pass2 = document.getElementById('registerprivate:pass2');
     // Setting colors (green and red) for visuals
     var goodColor = "#66cc66";
     var badColor = "#ff6666";
@@ -21,13 +19,32 @@ function checkPass()
         if (pass1.value === pass2.value) {
             // The passwords match, and the green color is set as feedback
             pass2.style.backgroundColor = goodColor;
-            message.style.color = goodColor;
-            message.innerHTML = "Passwords Match!";
         } else {
             // The passwords don't match, and the red color is used as feedback
             pass2.style.backgroundColor = badColor;
-            message.style.color = badColor;
-            message.innerHTML = "Passwords Do Not Match!";
+        }
+    }
+}
+
+function checkPassCorporate()
+{
+    // Stores password fields as objects
+    var pass3 = document.getElementById('registercorporate:pass3');
+    var pass4 = document.getElementById('registercorporate:pass4');
+    // Setting colors (green and red) for visuals
+    var goodColor = "#66cc66";
+    var badColor = "#ff6666";
+
+    // Check if both password fields are filled
+    if (pass3.value !== "" && pass4.value !== "") {
+
+        // Compares the values in the password fields
+        if (pass3.value === pass4.value) {
+            // The passwords match, and the green color is set as feedback
+            pass4.style.backgroundColor = goodColor;
+        } else {
+            // The passwords don't match, and the red color is used as feedback
+            pass4.style.backgroundColor = badColor;
         }
     }
 }
