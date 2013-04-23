aba83b801933ec1e17342da055b2b1bee0852fa9
l1nnk <linnkri91@gmail.com>
2013-04-23 12:35:58 +0200

    Popup product_overview.xhtml

--- HealthyCateringWeb/web/resources/js/product_overview_jq.js
+++ HealthyCateringWeb/web/resources/js/product_overview_jq.js
@@ -16,8 +16,24 @@ $(document).ready(function() {
         $("#popoup_add_product").hide();
     }
     
-    if (!show_Add_product && !show_edit_product) {
+    if (!show_add_product && !show_edit_product) {
         $("#site_opaque").hide();
     }
+
+    $("#product_tab_left h3").click(function() {
+        $("#product_tab_left").css("background-color", "#333333");
+        $("#product_tab_right").css("background-color", "#555555");
+        $("#add_product_package").fadeOut('fast', function(){
+            $("#add_product_single").fadeIn('fast');
+        });
+    });
+
+    $("#product_tab_right h3").click(function() {
+        $("#product_tab_left").css("background-color", "#555555");
+        $("#product_tab_right").css("background-color", "#333333");
+        $("#add_product_single").fadeOut('fast', function(){
+            $("#add_product_package").fadeIn('fast');
+        });
+    });
 
 });
