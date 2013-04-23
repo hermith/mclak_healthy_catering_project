d8887285b4cc88aaa730bdcaa5bc7de5d6a37701
Karl O <karloveraa@gmail.com>
2013-04-23 14:33:39 +0200

    Working on order_overview

--- HealthyCateringWeb/web/resources/js/active_orders_jq.js
+++ HealthyCateringWeb/web/resources/js/active_orders_jq.js
@@ -1,8 +1,8 @@
-
-
+var show_active = true;
 $(document).ready(function() {
 
     var show_popup = document.getElementById("active_orders_form:show_customer_detail").value === "true";
+    var show_active = true;
     if (show_popup) {
         $("#detail_order_info").show();
         $("#site_opaque").show();
@@ -10,21 +10,37 @@ $(document).ready(function() {
         $("#detail_order_info").hide();
         $("#site_opaque").hide();
     }
+    
+    if(show_active) {
+        show_left();
+    } else {
+        show_right();
+    }
 
     $("#order_tab_left h3").click(function() {
-        $("#order_tab_left").css("background-color", "#333333").fadeTo('fast');
-        $("#order_tab_right").css("background-color", "#555555");
-        $("#order_order_history").slideUp('fast');
-        $("#order_active_orders").slideDown('fast');
+        show_left();
+        show_active = true;
     });
 
     $("#order_tab_right h3").click(function() {
-        $("#order_tab_left").css("background-color", "#555555");
-        $("#order_tab_right").css("background-color", "#333333");
-        $("#order_active_orders").slideUp('fast');
-        $("#order_order_history").slideDown('fast');
+        show_right();
+        show_active = false;
     });
 
 
 
 });
+
+function show_left() {
+    $("#order_tab_left").css("background-color", "#333333").fadeTo('fast');
+    $("#order_tab_right").css("background-color", "#555555");
+    $("#order_order_history").slideUp('fast');
+    $("#order_active_orders").slideDown('fast');
+}
+
+function show_right() {
+    $("#order_tab_left").css("background-color", "#555555");
+    $("#order_tab_right").css("background-color", "#333333");
+    $("#order_active_orders").slideUp('fast');
+    $("#order_order_history").slideDown('fast');
+}
