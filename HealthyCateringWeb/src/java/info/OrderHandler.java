259c815276ecfa3653c166f73c208de5371574a0
Karl O <karloveraa@gmail.com>
2013-04-23 13:39:56 +0200

    order_overview now gets info from the database.

--- HealthyCateringWeb/src/java/info/OrderHandler.java
+++ HealthyCateringWeb/src/java/info/OrderHandler.java
@@ -2,23 +2,35 @@ package info;
 
 import database.DatabaseHandler;
 import java.util.ArrayList;
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import shopping.customer.Customer;
 
 /**
  * @author colsen91
  */
+
+@ApplicationScoped
 public class OrderHandler {
 
     @Inject
     DatabaseHandler db;
 
-    /*public ArrayList<Order> getActiveOrders() {
-        return db.selectOrders(true);
+    public ArrayList<Order> getActiveOrders() {
+        return db.selectUndeliveredOrders();
     }
 
     public ArrayList<Order> getOrderHistory() {
-        return db.selectOrders(false);
-    }*/
+        return db.selectOrders();
+    }
+
+    public Order getOrderFromID(int orderID) {
+        return db.selectOrder(orderID);
+    }
+
+    public Customer getCustomerFromID(int customerID) {
+        return db.selectCustomer(customerID);
+    }
     
     
 }
