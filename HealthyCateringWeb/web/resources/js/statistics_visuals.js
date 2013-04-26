e93c21b00b597e3046f612860ac12f08cd67e6d2
Aleksander L. Rasch <yakushi.san@gmail.com>
2013-04-26 11:38:53 +0200

    Wider color spectre for pie chart

--- HealthyCateringWeb/web/resources/js/statistics_visuals.js
+++ HealthyCateringWeb/web/resources/js/statistics_visuals.js
@@ -6,7 +6,6 @@ function drawGraph() {
     var numbers = document.getElementById("stat_form:orderstring").value;
     var split = numbers.split(" ");
     var myData = new Array();
-
     for (var i = 0; i < split.length - 1; i += 2) {
         myData.push([parseFloat(split[i]), parseFloat(split[i + 1])]);
     }
@@ -14,6 +13,9 @@ function drawGraph() {
     var myChart = new JSChart('graph', 'line');
     myChart.setDataArray(myData);
     myChart.setTitleColor('#000000');
+    myChart.setAxisNameX('Order IDs');
+    myChart.setAxisNameY('Order total price');
+    myChart.setPieUnitsColor('#000000');
     myChart.draw();
 }
 
@@ -21,17 +23,15 @@ function drawPieChart() {
     var numbers = document.getElementById("stat_form:productstring").value;
     var split = numbers.split(" ");
     var myData = new Array();
-
-    for (var i = 0; i < split.length-1; i += 2) {
+    for (var i = 0; i < split.length - 1; i += 2) {
         myData.push(['Product ID: ' + parseFloat(split[i]), parseFloat(split[i + 1])]);
     }
-    
+
     var colors = [];
-    
-    for(var i = 0; i < myData.length; i++){
+    for (var i = 0; i < myData.length; i++) {
         colors.push(getRandomColor());
     }
-    
+
     var myChart = new JSChart('chart', 'pie');
     myChart.setDataArray(myData);
     myChart.colorizePie(colors);
@@ -41,12 +41,32 @@ function drawPieChart() {
     myChart.draw();
 }
 
-function getRandomColor(){
+function getRandomColor() {
     var color = "#";
-    
-    for(var i = 0; i < 3; i++){
-        color += Math.floor(Math.random()*10);
+    var newChar;
+    for (var i = 0; i < 3; i++) {
+        newChar = Math.floor(Math.random() * 16);
+        if(newChar === 10){
+            newChar = 'a';
+        }
+        if(newChar === 11){
+            newChar = 'b';
+        }
+        if(newChar === 12){
+            newChar = 'c';
+        }
+        if(newChar === 13){
+            newChar = 'd';
+        }
+        if(newChar === 14){
+            newChar = 'e';
+        }
+        if(newChar === 15){
+            newChar = 'f';
+        }
+        color += newChar;
+        
     }
-    
+
     return color;
 }
