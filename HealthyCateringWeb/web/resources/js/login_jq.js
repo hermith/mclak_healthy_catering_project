8af47c5b666c74e4be2a0188d0376dc3cb154f5c
Karl O <karloveraa@gmail.com>
2013-04-26 17:05:58 +0200

    Fixed a lot of small things on the bug list

--- HealthyCateringWeb/web/resources/js/login_jq.js
+++ HealthyCateringWeb/web/resources/js/login_jq.js
@@ -8,13 +8,13 @@ var global_color_selected = "#183152";
 
 $(document).ready(function() {
     var isUp = true;
-    
+
     loggedIn = document.getElementById("hidden_login_field:is_user_logged_in").value === "true";
     loggedInAsPrivate = document.getElementById("hidden_login_field:is_user_logged_in_as_private").value === "true";
     loggedInAsCorporate = document.getElementById("hidden_login_field:is_user_logged_in_as_corporate").value === "true";
     wrongPassword = document.getElementById("hidden_login_field:last_login_failed").value === "true";
-    
-    if(wrongPassword) {
+
+    if (wrongPassword) {
         $("#top_bar_span_login").show();
         $("#site_opaque").show();
     } else {
@@ -23,41 +23,34 @@ $(document).ready(function() {
     }
 
     $("#top_bar_span_login_always_show").click(function() {
-        
+
         if (!loggedIn) {
-            // Starting animation of drop down and white overlay.
             $("#site_opaque").fadeToggle('slow');
             $("#top_bar_span_login").slideToggle('slow');
-
-            // Enabling/disabling overflow (scroll bars)
-            if (isUp) {
-                //$("body").css("overflow", "hidden");
-                isUp = false;
-            } else {
-                $("#top_bar_span_login").promise().done(function() {
-                    //$("body").css("overflow", "auto").fadeIn('slow');
-                    isUp = true;
-                });
-            }
         }
 
-    }).children().click(function(e) {
+    }).children().click(function() {
         return false;
     });
-    
+
+    $("#site_opaque").click(function() {
+        $("#top_bar_span_login").slideUp('slow');
+        $("#site_opaque").fadeToggle('slow');
+    });
+
     $("#forgot_password_link").click(function() {
         $(".login_form").fadeOut('fast', function() {
             $(".forgot_password").fadeIn('slow');
         });
     });
-    
+
     $("#forgot_password_back").click(function() {
         $(".forgot_password").fadeOut('fast', function() {
             $(".login_form").fadeIn('slow');
         });
     });
-    
-    if(loggedInAsPrivate || loggedInAsCorporate) {
+
+    if (loggedInAsPrivate || loggedInAsCorporate) {
         $("article#primary_content").css("width", "685px");
         $("aside#sidebar").show();
     } else {
