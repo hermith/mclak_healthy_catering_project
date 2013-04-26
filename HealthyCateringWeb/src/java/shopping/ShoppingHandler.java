776c711eae3253a1fba3edd6a67b158b4453492e
l1nnk <linnkri91@gmail.com>
2013-04-26 13:15:24 +0200

    ShoppingHandler.java
    - comments
    - getMenu() only returns active products

--- HealthyCateringWeb/src/java/shopping/ShoppingHandler.java
+++ HealthyCateringWeb/src/java/shopping/ShoppingHandler.java
@@ -15,68 +15,126 @@ import shopping.product.Product;
  * @author colsen91
  */
 @ApplicationScoped
-public class ShoppingHandler implements Serializable{
+public class ShoppingHandler implements Serializable {
 
     @Inject
     private DatabaseHandler database;
     private ArrayList<Product> products;
-    
+
     public ShoppingHandler() {
         products = new ArrayList<Product>();
     }
 
+    /**
+     * Calls insertOrder(order) in DatabaseHandler.java. Inserts a order in the
+     * database.
+     *
+     * @param order
+     * @return if the order was inserted.
+     */
     public boolean insertOrder(Order order) {
         return database.insertOrder(order);
     }
 
+    /**
+     * Calls selectProducts in DatabaseHandler.java if the variable
+     * productsTableHasChanged is given.
+     *
+     * @return a list with active products.
+     */
     public ArrayList<Product> getMenu() {
-        if(database.hasProductsTableChanged()){
+        ArrayList<Product> activeProducts = new ArrayList<Product>();
+        if (database.hasProductsTableChanged()) {
             products = database.selectProducts();
             database.setProductsTableChanged(false);
         }
-        return products;
+        for (Product p : products) {
+            if (p.isActive()) {
+                activeProducts.add(p);
+            }
+        }
+        return activeProducts;
     }
-    
-    public boolean insertCustomer(Customer customer, String username){
-        return(database.insertCustomer(customer, username));
+
+    /**
+     * Calls insertCustomer(customer,username) in DatabaseHandler. Inserts a new
+     * customer in the database.
+     *
+     * @param customer
+     * @param username
+     * @return if the customer was inserted.
+     */
+    public boolean insertCustomer(Customer customer, String username) {
+        return (database.insertCustomer(customer, username));
     }
-    
+
+    /**
+     * Calls selectCustomer(username) in DatabaseHandler.java.
+     *
+     * @param username
+     * @return Customer-object with the correct username.
+     */
     public Customer getCustomer(String username) {
         return database.selectCustomer(username);
     }
-    
+
+    /**
+     * Calls updateCustomer(customer) in DatabaseHandler.java. Updates the
+     * customer object in the database.
+     *
+     * @param customer
+     * @return whether the customer was updated.
+     */
     public boolean updateCustomer(Customer customer) {
         return database.updateCustomer(customer);
     }
-    
+
+    /**
+     * Fix newCustomer object with approved values.
+     *
+     * @param customer
+     * @param newCustomer
+     * @return whether the customer was updated.
+     */
     public boolean fixCustomer(Customer customer, Customer newCustomer) {
-        if(customer instanceof PrivateCustomer) {
+        if (customer instanceof PrivateCustomer) {
             PrivateCustomer privateCustomer = (PrivateCustomer) newCustomer;
             PrivateCustomer privateCustomerDatabase = (PrivateCustomer) customer;
-            if(privateCustomer.getFirstName().equals("")) {
+            if (privateCustomer.getFirstName().equals("")) {
                 privateCustomer.setFirstName(privateCustomerDatabase.getFirstName());
-            }if(privateCustomer.getLastName().equals("")) {
+            }
+            if (privateCustomer.getLastName().equals("")) {
                 privateCustomer.setLastName(privateCustomerDatabase.getLastName());
             }
-        }else if(customer instanceof CorporateCustomer) {
+        } else if (customer instanceof CorporateCustomer) {
             CorporateCustomer corporateCustomer = (CorporateCustomer) newCustomer;
             CorporateCustomer corporateCustomerDatabase = (CorporateCustomer) customer;
-            if(corporateCustomer.getCompanyName().equals("")) {
+            if (corporateCustomer.getCompanyName().equals("")) {
                 corporateCustomer.setCompanyName(corporateCustomerDatabase.getCompanyName());
             }
-        }if(newCustomer.getAddress().equals("")){
+        }
+        if (newCustomer.getAddress().equals("")) {
             newCustomer.setAddress(customer.getAddress());
-        }if(newCustomer.getEmail().equals("")){
+        }
+        if (newCustomer.getEmail().equals("")) {
             newCustomer.setEmail(customer.getEmail());
-        }if(newCustomer.getPhoneNumber().equals("")){
+        }
+        if (newCustomer.getPhoneNumber().equals("")) {
             newCustomer.setPhoneNumber(customer.getPhoneNumber());
-        }if(newCustomer.getCity().equals("")){
+        }
+        if (newCustomer.getCity().equals("")) {
             newCustomer.setCity(customer.getCity());
         }
         return updateCustomer(newCustomer);
     }
-    
-    public ArrayList<Order> getOrderHistory(int customerId){
+
+    /**
+     * Calls selectOrders(customerId) in DatabaseHandler.java.
+     *
+     * @param customerId
+     * @return order history to the given customer id.
+     */
+    public ArrayList<Order> getOrderHistory(int customerId) {
         return database.selectOrders(customerId);
     }
 }
