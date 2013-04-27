b3fddf9d6fbe1a22ff1964b2019b5f7a8c36e7b4
l1nnk <linnkri91@gmail.com>
2013-04-27 19:04:04 +0200

    Comments in package info.customer

--- HealthyCateringWeb/src/java/info/customer/CustomerHandler.java
+++ HealthyCateringWeb/src/java/info/customer/CustomerHandler.java
@@ -9,48 +9,80 @@ import shopping.customer.Customer;
 import shopping.customer.PrivateCustomer;
 
 /**
- * @author colsen91
+ * CustomerHandler is used to handle logic related to customers. Mainly
+ * functions for employees.
+ *
+ * @author Linn Kristin
  */
 @ApplicationScoped
 public class CustomerHandler {
+
     @Inject
     private DatabaseHandler databaseHandler;
-    
+
+    /**
+     * Calls selectCustomers() in DatabaseHandler.
+     *
+     * @return Array with all customers in the database.
+     */
     public ArrayList<Customer> getAllCustomers() {
         return databaseHandler.selectCustomers();
     }
-    
+
+    /**
+     * Calls selectCustomer(id) in DatabaseHandler.java.
+     *
+     * @param id
+     * @return Customer-object with correct id.
+     */
     public Customer selectCustomer(int id) {
         return databaseHandler.selectCustomer(id);
     }
-    
+
+    /**
+     * Calls updateCustomer(customer) in DatabaseHandler.java.
+     *
+     * @param customer
+     * @return True if all changes were made and no errors occured.
+     */
     public boolean updateCustomer(Customer customer) {
         return databaseHandler.updateCustomer(customer);
     }
-    
+
+    /**
+     * Fixes the customer and calls updateCustomer(customer).
+     *
+     * @param customer
+     * @return True if all changes were made and no error occured.
+     */
     public boolean fixCustomer(Customer customer) {
         Customer customerDatabase = selectCustomer(customer.getCustomerId());
-        if(customer instanceof PrivateCustomer) {
+        if (customer instanceof PrivateCustomer) {
             PrivateCustomer privateCustomer = (PrivateCustomer) customer;
             PrivateCustomer privateCustomerDatabase = (PrivateCustomer) customerDatabase;
-            if(privateCustomer.getFirstName().equals("")) {
+            if (privateCustomer.getFirstName().equals("")) {
                 privateCustomer.setFirstName(privateCustomerDatabase.getFirstName());
-            }if(privateCustomer.getLastName().equals("")) {
+            }
+            if (privateCustomer.getLastName().equals("")) {
                 privateCustomer.setLastName(privateCustomerDatabase.getLastName());
             }
-        }else if(customer instanceof CorporateCustomer) {
+        } else if (customer instanceof CorporateCustomer) {
             CorporateCustomer corporateCustomer = (CorporateCustomer) customer;
             CorporateCustomer corporateCustomerDatabase = (CorporateCustomer) customerDatabase;
-            if(corporateCustomer.getCompanyName().equals("")) {
+            if (corporateCustomer.getCompanyName().equals("")) {
                 corporateCustomer.setCompanyName(corporateCustomerDatabase.getCompanyName());
             }
-        }if(customer.getAddress().equals("")){
+        }
+        if (customer.getAddress().equals("")) {
             customer.setAddress(customerDatabase.getAddress());
-        }if(customer.getEmail().equals("")){
+        }
+        if (customer.getEmail().equals("")) {
             customer.setEmail(customerDatabase.getEmail());
-        }if(customer.getPhoneNumber().equals("")){
+        }
+        if (customer.getPhoneNumber().equals("")) {
             customer.setPhoneNumber(customerDatabase.getPhoneNumber());
-        }if(customer.getCity().equals("")){
+        }
+        if (customer.getCity().equals("")) {
             customer.setCity(customerDatabase.getCity());
         }
         return updateCustomer(customer);
