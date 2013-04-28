544783d52fce23c2c1a9f9c2ebc50cff219e7640
Karl O <karloveraa@gmail.com>
2013-04-28 14:21:35 +0200

    Fixed two small javaDoc errors in GalleriaBean and StatisticsBean

--- HealthyCateringWeb/src/java/image/GalleriaBean.java
+++ HealthyCateringWeb/src/java/image/GalleriaBean.java
@@ -1,62 +1,57 @@
-
-
-
 package image;
 
-/**
- * This file adds images into a gallery with diffrent titles
- * in different language
- * 
- * @author Maria
- */
-
-import java.util.ArrayList;  
-import java.util.List;  
-import java.util.Locale; 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
+/**
+ * This file adds images into a gallery with diffrent titles in different
+ * language
+ *
+ * @author Maria
+ */
 @Named
 public class GalleriaBean {
+
     private List<GalleriaImages> images_no;
     private List<GalleriaImages> images_en;
-    
     private String[] foodnames_no = {null, "Biff", "Laks", "Gresk salat", "Ferske råvarer", "Kyllingbryst"};
     private String[] foodnames_en = {null, "Steak", "Salmon", "Greek salad", "Fresh ingredients", "Chicken breast"};
-    
-    
-     
-    public GalleriaBean() {  
+
+    public GalleriaBean() {
         images_no = new ArrayList<GalleriaImages>();
-        for(int i=1; i<=5; i++){
+        for (int i = 1; i <= 5; i++) {
             images_no.add(new GalleriaImages(foodnames_no[i], "food" + i + ".jpg"));
         }
-        
-         images_en = new ArrayList<GalleriaImages>();
-        for(int i=1; i<=5; i++){
+
+        images_en = new ArrayList<GalleriaImages>();
+        for (int i = 1; i <= 5; i++) {
             images_en.add(new GalleriaImages(foodnames_en[i], "food" + i + ".jpg"));
-   
+
         }
     }
-    
+
     /**
      * Get images in different language
+     *
      * @return images
      */
-    public List<GalleriaImages> getImages_no() {  
-        return images_no;  
-    } 
-    
-    public List<GalleriaImages> getImages_en() {  
-        return images_en;  
-    } 
-    
+    public List<GalleriaImages> getImages_no() {
+        return images_no;
+    }
+
+    public List<GalleriaImages> getImages_en() {
+        return images_en;
+    }
+
     /**
      * Check for current language
+     *
      * @return current language
      */
-    
-    public boolean isNo(){
+    public boolean isNo() {
         FacesContext context = FacesContext.getCurrentInstance();
         Locale locale = context.getViewRoot().getLocale();
         return locale.getLanguage().equals("no");
